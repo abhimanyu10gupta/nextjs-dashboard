44f0603c6a50a7c4ca2091e7890eab87cd92f013
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2024-07-21 22:59:02 +1000

    add metadata to invoice edit

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,9 +4,12 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
  
+ 
 export const metadata: Metadata = {
   title: 'Invoices',
+  description: 'All Invoices for customers'
 };
+
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
